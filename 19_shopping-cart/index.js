09c9acfe50ddb8a2bd2c8aa0806878a0c6953d36
Ivy Chen <36115038+ivavay@users.noreply.github.com>
2023-08-03 18:59:57 +0800

    Update index.js

--- 19_shopping-cart/index.js
+++ 19_shopping-cart/index.js
@@ -7,6 +7,7 @@ Then find a method that will round the total to 2 decimal places.
 Example output: 73.44
 */
 
+// my solution 
 function total(arr){
     let numbers =[];
     
@@ -22,5 +23,15 @@ function total(arr){
     return total
     
 }
+
+// the course's solution (which makes more sense, without the need for loop) 
+
+// function total(arr){
+//     const total = arr.reduce((acc, curr) => {
+//         return acc + curr.price;
+//     }, 0);
+    
+//    return total.toFixed(2);
+// }
 
 console.log(total(shoppingCart));
